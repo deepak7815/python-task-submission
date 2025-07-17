edc55b053002c920581183b75cfbc289a2d259f0
Deepak Kumar Dutta <deepakdutta7815@gmail.com>
2025-07-17 12:44:11 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,18 @@
+number1= 75
+number2= 54
+
+#addition
+
+print(number1 + number2)
+
+#subtraction
+
+print(number1 - number2)
+
+#multiplication
+
+print(number1 * number2)
+
+#division
+
+print(number1 / number2)
