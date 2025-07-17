e94cf2eaf94dcbbe79e3a100c1c073c167b3180e
Deepak Kumar Dutta <deepakdutta7815@gmail.com>
2025-07-17 13:19:17 +0530

    Add files via upload

--- Task 2.py
+++ Task 2.py
@@ -1,3 +1,26 @@
+#task 1
+
+number1= 75
+number2= 54
+
+#addition
+
+print(number1 + number2)
+
+#subtraction
+
+print(number1 - number2)
+
+#multiplication
+
+print(number1 * number2)
+
+#division
+
+print(number1 / number2)
+
+
+#task 2
 
 #first name : deepak
 #last name  : dutta
